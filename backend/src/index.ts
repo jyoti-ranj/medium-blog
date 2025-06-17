e38c284cb42a94666518af29a239a565c1e3a0f9
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2025-06-17 12:10:36 +0530

    intializing the endpoints of medium-blog

---
+++ backend/src/index.ts
@@ -0,0 +1,76 @@
+import { Hono } from 'hono'
+import { PrismaClient } from './generated/prisma/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode , jwt, sign , verify } from 'hono/jwt';
+import bcrypt from 'bcryptjs'
+
+
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+    SECRET_KEY:string
+	}
+}>();
+
+
+app.post('/api/v1/signup', async(c) => {
+ const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate());
+
+ const body = await c.req.json();
+
+ const hashedPassword = await bcrypt.hash(body.password , 10)
+ const user = await prisma.user.create({
+  data: {
+    email: body.email,
+    password: hashedPassword,
+  },
+ })
+  
+  const token = await sign({id:user.id} , c.env.SECRET_KEY);
+  
+
+ return c.json({ message: 'User created successfully' , token });
+})
+
+app.post('/api/v1/signin', async(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body =await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+     email: body.email
+    }
+  })
+  if(!user){
+    return c.json({ message: 'User not found' });
+  }
+  const isValid = await bcrypt.compare(body.password, user.password);
+
+ if(!isValid){
+  return c.json({ message: 'Invalid password' },401);
+}
+  const token = await sign({id:user.id} , c.env.SECRET_KEY);
+
+ return c.json({
+  message: `signin successfully, welcome back! ${body.email}`,
+  token
+}, 200);
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
